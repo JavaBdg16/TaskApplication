bad306b0aacee4e050f3f346f722e576f02400a6
Marcin Szupke <marcin.szupke.sda@gmail.com>
2022-02-13 13:43:27 +0100

    password encoder

--- src/main/java/pl/sda/taskapplication/DataLoader.java
+++ src/main/java/pl/sda/taskapplication/DataLoader.java
@@ -3,6 +3,7 @@ package pl.sda.taskapplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import pl.sda.taskapplication.entity.Comment;
 import pl.sda.taskapplication.entity.Task;
@@ -28,12 +29,15 @@ public class DataLoader implements ApplicationRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
         User user = new User();
         user.setUsername("jnowak");
-        user.setPassword("1234");
+        user.setPassword(passwordEncoder.encode("1234"));
 
         userRepository.save(user);
 
